e01417d334e54053a0d83c42578112d52d13516a
Nayan Mevada <nayanmevada@ymail.com>
2019-10-14 23:52:02 +0530

    v1

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn TodoDjango.wsgi --log-file -
+web: gunicorn backend.wsgi --log-file -
--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 # import dj_database_url 
 # prod_db  =  dj_database_url.config(conn_max_age=500)
 # DATABASES['default'].update(prod_db)
@@ -129,3 +130,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 CORS_ORIGIN_ALLOW_ALL = True
+
+django_heroku.settings(locals())
