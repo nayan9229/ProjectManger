eb3d7baa6c0b45d60d0c94b4f3fc1ad6edc55369
nayan <nayanmevada@ymail.com>
2019-10-15 04:35:29 +0000

    update

--- frontend/servers.js
+++ frontend/servers.js
@@ -1,7 +1,8 @@
 var StaticServer = require('static-server');
+const port = process.env.PORT || 8090;
 var server = new StaticServer({
   rootPath: '.',            // required, the root of the server file tree
-  port: 8080,               // required, the port to listen
+  port: port,               // required, the port to listen
   name: 'my-http-server',   // optional, will set "X-Powered-by" HTTP header
 //   host: '10.0.0.100',       // optional, defaults to any interface
   cors: '*',                // optional, defaults to undefined
