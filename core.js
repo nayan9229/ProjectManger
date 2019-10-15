3edf900edcf07f83c567a9feb9019618ac965388
Nayan Mevada <nayanmevada@ymail.com>
2019-10-15 22:30:40 +0530

    Update

--- core.js
+++ core.js
@@ -15,7 +15,7 @@ app.config(function ($routeProvider, $locationProvider) {
         })
     $locationProvider.html5Mode(true)
 });
-app.controller('mainController', function ($scope, $http) {
+app.controller('mainController', function ($scope, $http, $location) {
     $scope.formData = {};
     $scope.newProject = {};
     $scope.isLoading = true;
@@ -64,7 +64,67 @@ app.controller('mainController', function ($scope, $http) {
         $http.get(`${baseURL}/api/projects`)
             .success(function (data) {
                 $scope.projects = data;
+                $http.get(`${baseURL}/api/todos`)
+                    .success(function (data) {
+                        $scope.projects.forEach(p => {
+                            let count = 0;
+                            data.forEach(d => {
+                                // if (count < 5) {
+                                    if (p.id == d.projectId) {
+                                        if (p.tasks) {
+                                            p.tasks.push(d);
+                                        } else {
+                                            p.tasks = [];
+                                            p.tasks.push(d);
+                                        }
+                                    }
+                                // }
+                                count++;
+                            });
+                            $scope.isLoading = false;
+                        });
+                    })
+                    .error(function (data) {
+                        console.log('Error: ' + data);
+                    });
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+            });
+    }
+
+    $scope.createReports = function () {
+        console.log($scope.newProject);
+        if(!$scope.newProject.avatar){
+            $scope.newProject.avatar = 'https://url'
+        }
+        $http.post(`${baseURL}/api/projects/`, $scope.newProject)
+            .success(function (data) {
+                $scope.newProject = {}; // clear the form so our user is ready to enter another
+                getProjects();
                 console.log(data);
+                $location.path("/");
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+                $location.path("/");
+            });
+    };
+
+});
+
+app.controller('projectController', function ($scope, $http, $routeParams, $location) {
+    $scope.formData = {};
+    $scope.newTask = {};
+    $scope.isLoading = true;
+    $scope.projectId = $routeParams.projectId;
+    $scope.project = {};
+    getProjects();
+
+    function getProjects() {
+        $http.get(`${baseURL}/api/projects`)
+            .success(function (data) {
+                $scope.projects = data;
                 $http.get(`${baseURL}/api/todos`)
                     .success(function (data) {
                         $scope.projects.forEach(p => {
@@ -80,11 +140,11 @@ app.controller('mainController', function ($scope, $http) {
                             });
                             $scope.isLoading = false;
                         });
-
-                        console.log(data);
+                        getProjectById();
                     })
                     .error(function (data) {
                         console.log('Error: ' + data);
+                        getProjectById();
                     });
             })
             .error(function (data) {
@@ -92,32 +152,48 @@ app.controller('mainController', function ($scope, $http) {
             });
     }
 
-    $scope.createReports = function () {
-        console.log($scope.newProject);
-        $http.post(`${baseURL}/api/projects/`, $scope.newProject)
+    function getProjectById() {
+        $scope.projects.forEach(p => {
+            if (p.id == $scope.projectId) {
+                $scope.project = p;
+            }
+        });
+    }
+
+    $scope.createTask = function () {
+        $scope.newTask.completed = false;
+        $scope.newTask.projectId = $scope.projectId;
+        $scope.newTask.taskId = 0;
+        $http.post(`${baseURL}/api/todos/`, $scope.newTask)
             .success(function (data) {
                 $scope.newProject = {}; // clear the form so our user is ready to enter another
                 getProjects();
-                console.log(data);
             })
             .error(function (data) {
                 console.log('Error: ' + data);
             });
+    }
+    $scope.todoSelected = function (task, tcomp) {
+        console.log('[TASK]', task);
+        console.log('[TASK]', tcomp);
+    }
+
+    $scope.createReports = function () {
+        console.log($scope.newProject);
+        if(!$scope.newProject.avatar){
+            $scope.newProject.avatar = 'https://url'
+        }
+        $http.post(`${baseURL}/api/projects/`, $scope.newProject)
+            .success(function (data) {
+                $scope.newProject = {}; // clear the form so our user is ready to enter another
+                getProjects();
+                $location.path("/");
+                console.log(data);
+            })
+            .error(function (data) {
+                console.log('Error: ' + data);
+                $location.path("/");
+            });
     };
 
-});
-
-app.controller('projectController', function ($scope, $http, $routeParams) {
-    $scope.formData = {};
-    $scope.newProject = {};
-    $scope.isLoading = true;
-    $scope.projectId = $routeParams.projectId;
-    var project = {};
-    $scope.projects.forEach(p =>{
-        if(p.id === $scope.projectId){
-            project = p;
-        }
-    });
-    $scope.projects = [];
-    $scope.projects.push(project);
 });
